116220163aa6776fb85ada2c439d46685a8509f3
Matt Wright <mttwright@gmail.com>
2019-12-10 23:42:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,13 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+    self
+  end
 
 
 end
