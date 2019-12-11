ea89ee99b1e4170a3984caff3dea69da3b5e555d
Matt Wright <mttwright@gmail.com>
2019-12-11 04:48:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,8 +71,11 @@ class Student
   
   def update
     sql = <<-SQL
-      UPDATE students SET name = ?
+      UPDATE students SET name = ?, grade = ?
+      WHERE id = ?;
     SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
 
 
 end
